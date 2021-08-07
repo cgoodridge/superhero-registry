bad261856ea48aed123b7eca4be4b37d35c7398a
Che' Goodridge <>
2021-08-07 01:24:22 -0400

    Added search and sort features

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
 import './App.css';
+import { useState, useEffect, useCallback } from 'react';
 import Search from "./components/Search";
 import AddHero from "./components/AddHero";
 import Grid from '@material-ui/core/Grid';
-import heroList from "./data.json";
 import HeroInfo from "./components/HeroInfo";
 
 const App = () => {
+  let [heroList, setHeroList] = useState([]);
+  let [query, setQuery] = useState("");
+  let [sortBy, setSortBy] = useState("name");
+  let [orderBy, setOrderBy] = useState("asc");
+
+  const filteredHeroes = heroList.filter(
+    item => {
+      return (
+        item.name.toLowerCase().includes(query.toLowerCase()) || 
+        item.alias.toLowerCase().includes(query.toLowerCase()) || 
+        item.notes.toLowerCase().includes(query.toLowerCase()) 
+      )
+    }
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    )
+  })
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+      setHeroList(data)
+    });
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">Superhero Registry</h1>
       <AddHero />
-      <Search />
+      <Search 
+        query={query} 
+        onQueryChange={myQuery => setQuery(myQuery)} 
+        orderBy={orderBy} 
+        onOrderByChange={mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}/>
 
       <Grid container>
-        {heroList.map((hero, key) => (
-          <HeroInfo key={hero.id} hero={hero}/>
+        {filteredHeroes.map((hero, key) => (
+          <HeroInfo key={hero.id} hero={hero} 
+          onDeleteHero={ heroId => setHeroList(heroList.filter(hero => hero.id != heroId))}/>
         ))} 
       </Grid>
     </div>
--- src/components/HeroInfo.js
+++ src/components/HeroInfo.js
@@ -4,12 +4,17 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import CardMedia from '@material-ui/core/CardMedia';
+import { BiTrash } from 'react-icons/bi';
 import Typography from '@material-ui/core/Typography';
 
 
-const HeroInfo = ({hero}) => {
+const HeroInfo = ({ hero, onDeleteHero }) => {
     return (
         <Grid item xs={12} m={6} sm={3} className="mb-3 mt-3 ml-3 mr-3">
+            <button type="button" onClick={ () => onDeleteHero(hero.id)}
+             className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <BiTrash />
+            </button>
             <Card className="card small">
                 <CardMedia
                 component="img"
